014176d1e988386947d4a7754198151c721cf6ca
Natasha <nca28@cornell.edu>
2021-03-11 20:23:37 -0500

    fixed currency thing

--- backend/src/currency.js
+++ backend/src/currency.js
@@ -1,13 +1,9 @@
 var https = require("https");
-var url = "https://free.currconv.com/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=4a9548e48427521a158b";
+const { config } = require('./config.js');
 
-// this and the URL both work (below where options is in the GET request)
-var options = {
-  host: 'free.currconv.com',
-  path: '/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=4a9548e48427521a158b'
-};
+var newURL = "https://free.currconv.com/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=" + config.key;
 
-https.get(options, function (res) {
+https.get(newURL, function (res) {
   var dataResponse = '';
   res.on('data', function (chunk) {
     dataResponse += chunk;
@@ -16,5 +12,8 @@ https.get(options, function (res) {
     var jsonResponse = JSON.parse(dataResponse);
     console.log("1 USD = " + jsonResponse.USD_GBP + " GBP");
   });
+  res.on('error', function (err) {
+    console.error(err);
+  })
   //error catching should probaby go here
 });
