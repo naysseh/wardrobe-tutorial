425e0266acd8dfaaf50e5dee836845247e544ee7
Natasha <nca28@cornell.edu>
2021-03-11 19:56:12 -0500

    edits to bitcoin + another practice w api request

--- backend/src/bitcoin.js
+++ backend/src/bitcoin.js
@@ -2,19 +2,20 @@ var http = require('http');
 var url = 'http://api.coindesk.com/v1/bpi/currentprice.json';
 
 http.get(url, function (res) {
+  //create var for the body of the response from URL
   var body = '';
 
+  //add on data to the body from the response
   res.on('data', function (chunk) {
     body += chunk;
   });
 
+  //end of data - go thru JSON and get correct val
   res.on('end', function () {
     var bitcoinRes = JSON.parse(body);
     var value = bitcoinRes.bpi.USD.rate;
     console.log("1 BTC = $" + value + " (USD)");
   });
 
-}).on('error', function (err) {
-  console.log("Error: ", err);
 });
 
---
+++ backend/src/currency.js
@@ -0,0 +1,20 @@
+var https = require("https");
+var url = "https://free.currconv.com/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=4a9548e48427521a158b";
+
+// this and the URL both work (below where options is in the GET request)
+var options = {
+  host: 'free.currconv.com',
+  path: '/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=4a9548e48427521a158b'
+};
+
+https.get(options, function (res) {
+  var dataResponse = '';
+  res.on('data', function (chunk) {
+    dataResponse += chunk;
+  });
+  res.on('end', function () {
+    var jsonResponse = JSON.parse(dataResponse);
+    console.log("1 USD = " + jsonResponse.USD_GBP + " GBP");
+  });
+  //error catching should probaby go here
+});
